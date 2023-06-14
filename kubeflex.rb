b73b32d250c916b2136177282513dd40868e8b93
pdettori <dettori@us.ibm.com>
2023-06-14 01:50:25 +0000

    Brew formula update for kubeflex version v0.1.0

--- kubeflex.rb
+++ kubeflex.rb
@@ -10,7 +10,7 @@ class Kubeflex < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/kubestellar/kubeflex/releases/download/v0.1.0/kubeflex_0.1.0_darwin_amd64.tar.gz"
-      sha256 "40f4b5dd6111add03161b46ef299d708bcad80a0b9169a2eba5bb87d4c365ed7"
+      sha256 "c31d0472f4f028a2fe5632595f4405666139bd010f900c818d1770cff97898ab"
 
       def install
         bin.install "bin/kflex"
@@ -18,7 +18,7 @@ class Kubeflex < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/kubestellar/kubeflex/releases/download/v0.1.0/kubeflex_0.1.0_darwin_arm64.tar.gz"
-      sha256 "864d14614287de6449401f657511949b5244edc0807fc2baf931f49a09b3c2c5"
+      sha256 "92d7f05a8a1d02e49dc53686750c6930f67b0d721f53a3a4fd8ef6d2bf2c2a06"
 
       def install
         bin.install "bin/kflex"
@@ -29,7 +29,7 @@ class Kubeflex < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/kubestellar/kubeflex/releases/download/v0.1.0/kubeflex_0.1.0_linux_arm64.tar.gz"
-      sha256 "f771f03b2eb7c58ec358d9f9186cfda65b0a49b7dd5d4739216536a7cfc9f169"
+      sha256 "cc5d650acac175fe09dab10a0ddaa83b2d368e87ceaa5ca646cbe38bb97ab06a"
 
       def install
         bin.install "bin/kflex"
@@ -37,7 +37,7 @@ class Kubeflex < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/kubestellar/kubeflex/releases/download/v0.1.0/kubeflex_0.1.0_linux_amd64.tar.gz"
-      sha256 "7b2d13ca68f309f5b9032fe5b5d8beedf64c7489dfa98d959deb256ed97d2e56"
+      sha256 "0e72ec79e209ecc208f4816ca42525ea445a46f44178a8bc54f04ad0f20934ae"
 
       def install
         bin.install "bin/kflex"
