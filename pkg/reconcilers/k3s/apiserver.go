1c704d186b6e1430fef4c34d93f75782ba1ce4cc
rxinui <rainui.ly@gmail.com>
2025-06-24 23:31:15 +0200

    fix(431): solve k3s-server pod error
    - fix: add securityContext to k3s-server pod
    - fix: reconcile k3s server namespace
    
    Signed-off-by: rxinui <rainui.ly@gmail.com>

--- pkg/reconcilers/k3s/apiserver.go
+++ pkg/reconcilers/k3s/apiserver.go
@@ -25,15 +25,21 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	clog "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
+// k3s constants
 const ServerName = "k3s-server"
 const ServerDockerImage = "rancher/k3s"
+const StorageName = "data-k3s-server"
+const StorageClassName = "standard" // kind default storage class name which is rancher/local-storage (same as k3s but different name)
+const StoragePath = "/data"
 
 // K3s API server
 // NOTE: k3s is a single binary containing apiserver, etcd, controller-manager... therefore `Server` refers to all components
@@ -46,6 +52,7 @@ func serverLabels() map[string]string {
 	return map[string]string{
 		"controller.kubeflex.dev/type":         string(tenancyv1alpha1.ControlPlaneTypeK3s),
 		"controller.kubeflex.dev/service-name": ServiceName,
+		"controller.kubeflex.dev/pvc-name":     StorageName,
 	}
 }
 
@@ -87,9 +94,36 @@ func NewServer(cpName string) (*appsv1.StatefulSet, error) {
 							Ports: []v1.ContainerPort{
 								{ContainerPort: shared.SecurePort},
 							},
+							VolumeMounts: []v1.VolumeMount{
+								// VolumeMount k3s data
+								{
+									Name:      StorageName,
+									MountPath: StoragePath,
+									ReadOnly:  false,
+								},
+							},
+							SecurityContext: &v1.SecurityContext{
+								// Required by $ServerDockerImage
+								Privileged: ptr.To(true),
+								// Required to write filesystem as privileged
+								ReadOnlyRootFilesystem: ptr.To(false),
+								// Additional security
+								SeccompProfile: &v1.SeccompProfile{
+									Type: v1.SeccompProfileTypeRuntimeDefault,
+								},
+							},
 						},
 					},
 					Volumes: []v1.Volume{
+						// Volume k3s data
+						{
+							Name: StorageName,
+							VolumeSource: v1.VolumeSource{
+								PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{
+									ClaimName: StorageName,
+								},
+							},
+						},
 						// Volume kubernetes certificates
 						{
 							Name: "k8s-certs",
@@ -116,17 +150,81 @@ func NewServer(cpName string) (*appsv1.StatefulSet, error) {
 	}, nil
 }
 
+// apiVersion: v1
+// kind: PersistentVolumeClaim
+// metadata:
+//
+//	name: local-path-pvc
+//	namespace: k3stest
+//
+// spec:
+//
+//	accessModes:
+//	  - ReadWriteOnce
+//	storageClassName: standard
+//	volumeMode: Filesystem
+//	resources:
+//	  requests:
+//	    storage: 2Gi
+func NewPVC(cpName string) (*v1.PersistentVolumeClaim, error) {
+	return &v1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      StorageName,
+			Namespace: GenerateSystemNamespaceName(cpName),
+			Labels:    serverLabels(),
+		},
+		Spec: v1.PersistentVolumeClaimSpec{
+			AccessModes: []v1.PersistentVolumeAccessMode{
+				v1.ReadWriteOnce,
+			},
+			Resources: v1.VolumeResourceRequirements{
+				Requests: v1.ResourceList{
+					v1.ResourceStorage: resource.MustParse("2Gi"),
+				},
+			},
+			StorageClassName: ptr.To(StorageClassName),
+		},
+	}, nil
+}
+
 // Reconcile k3s server
 // implements ControlPlaneReconciler
 // TODO to implement
 func (r *Server) Reconcile(ctx context.Context, hcp *tenancyv1alpha1.ControlPlane) (ctrl.Result, error) {
 	log := clog.FromContext(ctx)
+	// Get k3s pvc that is required for k3s server to run
+	k3sPVCObject := &v1.PersistentVolumeClaim{}
+	k3sPVCObjectKey := client.ObjectKey{Namespace: GenerateSystemNamespaceName(hcp.Name), Name: StorageName}
+	log.Info("k3s:server.go:Reconcile:", "k3sPVCObjectKey", k3sPVCObjectKey)
+	err := r.Client.Get(ctx, k3sPVCObjectKey, k3sPVCObject)
+	if err != nil {
+		log.Error(err, "k3s:server.go:Reconcile:r.Client.Get pvc failed")
+		r.BaseReconciler.UpdateStatusForSyncingError(ctx, hcp, err) // TODO: to change
+		if apierrors.IsNotFound(err) {
+			log.Error(err, "k3s:server.go:Reconcile:pvc is not found error")
+			log.Info("k3s:server.go:Reconcile:call NewServer() on k3sServerObject")
+			// Generate new k3s server
+			k3sPVCObject, _ = NewPVC(hcp.Name)
+			log.Info("k3s:server.go:Reconcile:call SetControllerReference on pvc")
+			// Set owner reference of the API server object
+			if err := controllerutil.SetControllerReference(hcp, k3sPVCObject, r.Scheme); err != nil {
+				log.Error(err, "k3s:server.go:Reconcile:SetControllerReference on pvc failed")
+				return ctrl.Result{}, err
+			}
+			// Create the k3s server
+			log.Info("k3s:server.go:Reconcile:call r.Client.Create on", "k3sPVCObject", k3sPVCObject)
+			if err = r.Client.Create(ctx, k3sPVCObject); err != nil {
+				log.Error(err, "k3s:server.go:Reconcile:r.Client.Create pvc failed")
+				return ctrl.Result{RequeueAfter: 10}, err
+			}
+		}
+		return ctrl.Result{}, err
+	}
 	// Get k3s server from hosting cluster and stored it in k3sServerObject
-
 	k3sServerObject := &appsv1.StatefulSet{}
 	k3sServerObjectKey := client.ObjectKey{Namespace: GenerateSystemNamespaceName(hcp.Name), Name: ServerName}
 	log.Info("k3s:server.go:Reconcile:", "k3sServerObjectKey", k3sServerObjectKey)
-	err := r.Client.Get(ctx, k3sServerObjectKey, k3sServerObject)
+	err = r.Client.Get(ctx, k3sServerObjectKey, k3sServerObject)
 	if err != nil {
 		log.Error(err, "k3s:server.go:Reconcile:r.Client.Get failed")
 		r.BaseReconciler.UpdateStatusForSyncingError(ctx, hcp, err) // TODO: to change
