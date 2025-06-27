247270854324ae41a70ac7350b3915d18f4a77bd
redpinecube <tara.chakkithara@icloud.com>
2025-06-27 02:09:13 -0500

    feat: assign control plane name to kubeflex extension
    Signed-off-by: redpinecube <tara.chakkithara@icloud.com>

---
+++ pkg/kubeconfig/kubeconfig_test.go
@@ -0,0 +1,57 @@
+package kubeconfig
+
+import (
+	"testing"
+	"k8s.io/client-go/tools/clientcmd/api"
+)
+
+// Setup and teardown helpers
+func setupTestKubeconfigWithContext(ctxName string) *api.Config {
+	kconf := api.NewConfig()
+	kconf.Contexts[ctxName] = &api.Context{
+		Cluster:  "test-cluster",
+		AuthInfo: "test-user",
+	}
+	return kconf
+}
+
+func teardownTestKubeconfig(kconf *api.Config, ctxName string) {
+	delete(kconf.Contexts, ctxName)
+}
+
+func TestAssignControlPlaneToContext_Positive(t *testing.T) {
+	ctxName := "test-context"
+	cpName := "test-controlplane"
+	kconf := setupTestKubeconfigWithContext(ctxName)
+	defer teardownTestKubeconfig(kconf, ctxName)
+
+	err := AssignControlPlaneToContext(kconf, cpName, ctxName)
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+
+	ext, ok := kconf.Contexts[ctxName].Extensions[ExtensionKubeflexKey]
+	if !ok {
+		t.Fatalf("expected kubeflex extension to be set in context")
+	}
+
+	runtimeExt, ok := ext.(*RuntimeKubeflexExtension)
+	if !ok {
+		t.Fatalf("expected extension to be of type *RuntimeKubeflexExtension")
+	}
+
+	if got := runtimeExt.Data[ExtensionControlPlaneName]; got != cpName {
+		t.Errorf("expected controlplane-name to be '%s', got '%s'", cpName, got)
+	}
+}
+
+func TestAssignControlPlaneToContext_Negative_ContextDoesNotExist(t *testing.T) {
+	ctxName := "nonexistent-context"
+	cpName := "test-controlplane"
+	kconf := api.NewConfig()
+
+	err := AssignControlPlaneToContext(kconf, cpName, ctxName)
+	if err == nil {
+		t.Fatalf("expected error for nonexistent context, got nil")
+	}
+} 
