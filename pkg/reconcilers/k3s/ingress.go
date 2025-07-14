ab56f14c678ec48cb076bc0fff8d5d7f5f275fc1
rxinui <rainui.ly@gmail.com>
2025-07-14 12:26:33 +0200

    new(450): add k3s ingress manifest as function
    Signed-off-by: rxinui <rainui.ly@gmail.com>

---
+++ pkg/reconcilers/k3s/ingress.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2023 The KubeStellar Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package k3s
+
+import (
+	// "context"
+	// "fmt"
+
+	// tenancyv1alpha1 "github.com/kubestellar/kubeflex/api/v1alpha1"
+	"github.com/kubestellar/kubeflex/pkg/reconcilers/shared"
+	// "github.com/kubestellar/kubeflex/pkg/util"
+	networkingv1 "k8s.io/api/networking/v1"
+	// apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+	// "sigs.k8s.io/controller-runtime/pkg/client"
+	// "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+const (
+	IngressClassNameNGINX = "nginx"
+)
+
+// NewIngress create k3s ingress to reach k3s apiserver from outside the cluster
+func NewIngress(host string, serviceName string) *networkingv1.Ingress {
+	return &networkingv1.Ingress{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Ingress",
+			APIVersion: "networking.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      ServerName,
+			Namespace: ServerSystemNamespace,
+			Annotations: map[string]string{
+				"nginx.ingress.kubernetes.io/ssl-passthrough": "true",
+			},
+		},
+		Spec: networkingv1.IngressSpec{
+			IngressClassName: ptr.To(IngressClassNameNGINX),
+			Rules: []networkingv1.IngressRule{
+				{
+					Host: host,
+					IngressRuleValue: networkingv1.IngressRuleValue{
+						HTTP: &networkingv1.HTTPIngressRuleValue{
+							Paths: []networkingv1.HTTPIngressPath{
+								{
+									PathType: ptr.To(networkingv1.PathTypePrefix),
+									Path:     "/",
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: serviceName,
+											Port: networkingv1.ServiceBackendPort{
+												Number: shared.DefaultPort,
+												Name:   shared.DefaultPortName,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
