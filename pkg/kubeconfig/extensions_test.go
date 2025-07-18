89dae3998b7c35ba210314a302adad28c9f91fcc
redpinecube <tara.chakkithara@icloud.com>
2025-07-17 21:27:04 -0500

    test : CheckContextScopeKubeflexExtensionSet no data
    Signed-off-by: redpinecube <tara.chakkithara@icloud.com>

--- pkg/kubeconfig/extensions_test.go
+++ pkg/kubeconfig/extensions_test.go
@@ -222,3 +222,24 @@ func TestCheckContextScopeKubeflexExtensionSetNoKubeflexExtensions(t *testing.T)
 		t.Errorf("Expected %s, got %s", DiagnosisStatusOK, result)
 	}
 }
+
+func TestCheckContextScopeKubeflexExtensionSetNoData(t *testing.T) {
+	kconf := api.NewConfig()
+	kconf.Clusters["cluster1"] = &api.Cluster{Server: "https://example.com:6443"}
+	kconf.AuthInfos["user1"] = &api.AuthInfo{Token: "token"}
+
+	ext := NewRuntimeKubeflexExtension()
+	ext.Data = nil
+
+	kconf.Contexts["ctx1"] = &api.Context{
+		Cluster:    "cluster1",
+		AuthInfo:   "user1",
+		Extensions: map[string]runtime.Object{ExtensionKubeflexKey: ext},
+	}
+	kconf.CurrentContext = "ctx1"
+
+	result := CheckContextScopeKubeflexExtensionSet(*kconf)
+	if result != DiagnosisStatusCritical {
+		t.Errorf("Expected %s, got %s", DiagnosisStatusCritical, result)
+	}
+}
