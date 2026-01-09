45c7d76edf13246c0b525e47267596349f9c7aff
Rishi Jat <rishijat098@gmail.com>
2026-01-09 23:07:47 +0530

    Fix: restore +kubebuilder:object:generate marker in doc.go for deepcopy generation
    Signed-off-by: Rishi Jat <rishijat098@gmail.com>

--- api/v1alpha1/doc.go
+++ api/v1alpha1/doc.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// +kubebuilder:object:generate=true
 // +groupName=tenancy.kflex.kubestellar.org
 
 // Package v1alpha1 contains API Schema definitions for the tenancy v1alpha1 API group
